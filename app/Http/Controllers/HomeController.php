1749c924c68783fd2caf053ecaa956a7677e3dbc
zumaidi permana <zumaidipermana26@gmail.com>
2023-04-09 23:47:59 +0700

    Latihan fundamental laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    
+    public function home()
+    {
+        return view('welcome');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    public function products()
+    {
+        return view('products');
+    }
+}
---
+++ resources/views/products.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('about') }}">About</a>
+    <a href="{{ route('contact') }}">Contact Us</a>
+    <a href="{{ route('products') }}">Products</a>
+</body>
+</html>
